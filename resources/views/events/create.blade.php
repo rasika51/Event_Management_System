02bbe270957a84b646f5ae93d1a18f7933d2bcec
Rasika Wedaarachchi <wedaarachchirasika@gmail.com>
2025-10-03 23:31:43 +0530

    Create Event Management - dashbord

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -1,7 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container-fluid py-4">
     <div class="row">
         <div class="col-12">
             <div class="d-flex justify-content-between align-items-center mb-4">
@@ -114,7 +113,6 @@
             </div>
         </div>
     </div>
-</div>
 
 @push('scripts')
 <script>
